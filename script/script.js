26bccf71e1017d4fc66c88c1ec1b48627134fca0
Igor G <igorogi.g@gmail.com>
2018-03-29 14:48:41 +0300

    esc

--- script/script.js
+++ script/script.js
@@ -8,11 +8,20 @@ link.addEventListener("click",function(event){
   event.preventDefault();
   console.log("ОТКРЫЛ ФОРМУ СВЯЗИ");
   popup.classList.add("popup-contact-show");
-});
+  popup.querySelector("[name=name]").focus();
+  });
 
 
 close.addEventListener("click",function(event){
   event.preventDefault();
   console.log("ЗАКРЫЛ ФОРМУ СВЯЗИ");
   popup.classList.remove("popup-contact-show");
+});
+
+window.addEventListener("keydown", function (event) {
+  if(event.keyCode==27) {
+    if(popup.classList.contains("popup-contact-show"))
+    {popup.classList.remove("popup-contact-show");
+    }
+  }
 });
